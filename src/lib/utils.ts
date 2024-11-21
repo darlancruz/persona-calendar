e1b26f5c9ff3c46ee1ec75683b1077230e7f5807
Darlan <darlan.silvadacruz@gmail.com>
2024-11-21 17:43:08 -0300

    create utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,18 @@
+export const months = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december",
+];
+
+export const days = ["Su", "M", "Tu", "W", "Th", "F", "Sa"];
+
+export const games = ["p3", "p3fes", "p3p", "p4", "p4g", "p5", "p5r"];
